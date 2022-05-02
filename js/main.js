9f92ea3f30e966ff6e6ff64cad76e70ac41a0dbf
VasoC <vasochkhartishvili@gmail.com>
2022-05-02 16:31:12 +0400

    Update!

--- js/main.js
+++ js/main.js
@@ -9,10 +9,51 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(validateEmail());
   console.log(emailInput.value);
+  console.log(validatepersonalnumber());
+  console.log(validatemobilenumberInput());
+  console.log(validateposition()());
 });
 function validateEmail() {
   if (emailInput.value) {
+    emailInput.classList.remove("has-error");
+    emailInput.parentNode.querySelector(".errortext").innerHTML = "";
     return true;
   }
+  emailInput.classList.add("has-error");
+  emailInput.parentNode.querySelector(".errortext").innerHTML = "Enter email";
+  return false;
+}
+
+function validatepersonalnumber() {
+  if (personalnumberInput.value) {
+    personalnumberInput.classList.remove("has-error");
+    personalnumberInput.parentNode.querySelector(".errortext").innerHTML = "";
+    return true;
+  }
+  personalnumberInput.classList.add("has-error");
+  personalnumberInput.parentNode.querySelector(".errortext").innerHTML =
+    "Enter Pin";
+  return false;
+}
+function validatemobilenumberInput() {
+  if (mobilenumberInput.value) {
+    mobilenumberInput.classList.remove("has-error");
+    mobilenumberInput.parentNode.querySelector("errortext").innerHTML = "";
+    return true;
+  }
+  mobilenumberInput.classList.add("has-error");
+  mobilenumberInput.parentNode.querySelector(".errortext").innerHTML =
+    "Enter mobile number";
+  return false;
+}
+function validateposition() {
+  if (positionInput.value) {
+    positionInput.classList.remove("has-error");
+    positionInput.parentNode.querySelector(".errortext").innerHTML = "";
+    return true;
+  }
+  positionInput.classList.add("has-error");
+  positionInput.parentNode.querySelector(".errortext").innerHTML =
+    "Enter your position";
   return false;
 }
